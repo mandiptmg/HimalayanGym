9018ae2f9964e9a5b1a3ca628585564f83fa839a
Mandip Tamang <mandiptamang159@gmail.com>
2024-01-07 12:45:54 +0545

    add fix button to reach the top

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,42 @@ import Testimonial from './components/Testinominal/Testimonial'
 import SignUp from './components/signup/SignUp'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import { LiaLongArrowAltUpSolid } from 'react-icons/lia'
+import { useEffect } from 'react'
 const App = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        document.querySelector('.look').classList.add('hidden')
+      } else {
+        document.querySelector('.look').classList.remove('hidden')
+      }
+    }
+    handleScroll()
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
   return (
     <div>
       <Navbar />
       <Home />
-      <About/>
-      <Services/>
-      <Choose/>
-      <Price/>
-      <Testimonial/>
-      <Faq/>
-      <SignUp/>
-      <Contact/>
-      <Footer/>
+      <About />
+      <Services />
+      <Choose />
+      <Price />
+      <Testimonial />
+      <Faq />
+      <SignUp />
+      <Contact />
+      <Footer />
+      <div className='fixed bottom-4 right-4'>
+        <button
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+          className='p-2 bg-red-600 text-white look rounded text-3xl '
+        >
+          <LiaLongArrowAltUpSolid />
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,7 +9,7 @@ import StatsComponent from './StatsComponent'
 const Home = () => {
   return (
     <div id='1'>
-      <div className='before:absolute before:-z-10 before:bg-[url(https://fitpulse.onlywebcoding.com.ua/images/header-img.png)] before:top-0 before:left-0 -mt-20 before:bg-no-repeat before:bg-top grid items-center before:h-screen h-screen before:w-full before:bg-blend-overlay before:bg-black/30  before:bg-cover'>
+      <div className='before:absolute before:-z-10 before:bg-[url(https://fitpulse.onlywebcoding.com.ua/images/header-img.png)] before:top-0 before:left-0 -mt-24 before:bg-no-repeat before:bg-top grid items-center before:h-screen h-screen before:w-full before:bg-blend-overlay before:bg-black/30  before:bg-cover'>
         <div className=' w-[90vw] md:container mx-auto'>
           <p className='leading-normal md:text-5xl text-4xl lg:text-7xl  font-bold '>
             STRENGTH IN MOTION <br />
